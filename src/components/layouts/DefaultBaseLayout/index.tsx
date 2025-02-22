2609760fa6568df1956df59c271c4ddad6337134
Jason Day <movemaine@gmail.com>
2025-02-22 11:33:46 -0500

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -5,10 +5,6 @@ import Header from '../../sections/Header';
 import Footer from '../../sections/Footer';
 import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } from '../../../utils/seo-utils';
 
-export const metadata = {
-  title: 'Next.js',
-}
-
 export default function DefaultBaseLayout(props) {
     const { page, site } = props;
     const { enableAnnotations = true } = site;
@@ -19,7 +15,19 @@ export default function DefaultBaseLayout(props) {
     return (
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
             <div className="sb-base sb-default-base-layout">
-               
+                <Head>
+                    <title>{title}</title>
+                    {metaDescription && <meta name="description" content={metaDescription} />}
+                    {metaTags.map((metaTag) => {
+                        if (metaTag.format === 'property') {
+                            // OpenGraph meta tags (og:*) should be have the format <meta property="og:…" content="…">
+                            return <meta key={metaTag.property} property={metaTag.property} content={metaTag.content} />;
+                        }
+                        return <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />;
+                    })}
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    {site.favicon && <link rel="icon" href={site.favicon} />}
+                </Head>
                 {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
                 {props.children}
                 {site.footer && <Footer {...site.footer} enableAnnotations={enableAnnotations} />}
@@ -27,3 +35,4 @@ export default function DefaultBaseLayout(props) {
         </div>
     );
 }
+
