4e27e608a35543831afb13294df4773f2f8b0e8b
Jason Day <movemaine@gmail.com>
2025-02-22 11:27:11 -0500

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -7,7 +7,7 @@ import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } fro
 
 
 export const metadata = {
-  title: 'test',
+  title: 'test'
 }
 
 export default function DefaultBaseLayout(props) {
