c14e5781550d926cf6f27daea17f0c0c9174d7cd
Jason Day <movemaine@gmail.com>
2025-02-22 11:30:29 -0500

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -5,9 +5,8 @@ import Header from '../../sections/Header';
 import Footer from '../../sections/Footer';
 import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } from '../../../utils/seo-utils';
 
-
 export const metadata = {
-  title: 'test'
+  title: 'Next.js',
 }
 
 export default function DefaultBaseLayout(props) {
@@ -20,10 +19,7 @@ export default function DefaultBaseLayout(props) {
     return (
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
             <div className="sb-base sb-default-base-layout">
-                <Head>
-                  
-                   
-                </Head>
+               
                 {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
                 {props.children}
                 {site.footer && <Footer {...site.footer} enableAnnotations={enableAnnotations} />}
